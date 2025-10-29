39c14985ef70f9882f5a612153172472c66bc529
PC3CSLab-R409 <antisubmitcodeincopy@gmail.com>
2025-10-29 16:38:54 +0530

    29102025 commit 4

--- Temperature Converter_2.c
+++ Temperature Converter_2.c
@@ -2,19 +2,19 @@
 int ctof(int celsius); // Function prototype
 int ftoc(int fahrenheit); // Function prototype
 int main() {
-    int choice, temp, convertedTemp;
+    int temp, convertedTemp;
+    char choice;
     printf("Temperature Converter\n");
-    printf("1. Celsius to Fahrenheit\n");
-    printf("2. Fahrenheit to Celsius\n");
-    printf("Enter your choice (1 or 2): ");
-    scanf("%d", &choice);
+    printf("1. (C)elsius to Fahrenheit\n");
+    printf("2. (F)ahrenheit to Celsius\n");
+    scanf("%c", &choice);
     
-    if (choice == 1) {
+    if (choice == 'C' || choice == 'c') {
         printf("Enter temperature in Celsius: ");
         scanf("%d", &temp);
         convertedTemp = ctof(temp);
         printf("Temperature in Fahrenheit: %d\n", convertedTemp);
-    } else if (choice == 2) {
+    } else if (choice == 'F' || choice == 'f') {
         printf("Enter temperature in Fahrenheit: ");
         scanf("%d", &temp);
         convertedTemp = ftoc(temp);
@@ -24,4 +24,12 @@ int main() {
     }
     
     return 0;
+}
+
+int ctof(int celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+int ftoc(int fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
 }
